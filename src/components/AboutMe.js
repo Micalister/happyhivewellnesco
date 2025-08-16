50604d075cc88a9dd0cae50f4245bdcbaef2a0c0
spilgr3 <spilgr3@wgu.edu>
2025-08-16 03:08:09 -0700

    About me 100% || Before and after needs work

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import '.././App.css';
 import Carousel from './Carousel.js';
-import AboutMeLeft from '.././assets/AboutMeLeft.svg';
-import AboutToPlans from '.././assets/AboutToPlans.svg';
+//import AboutMeLeft from '.././assets/AboutMeLeft.svg';
+//import AboutToPlans from '.././assets/AboutToPlans.svg';
+import aboutcapressa from '.././assets/aboutcapressa.svg';
 
 function AboutMe() {
     return (
         <div>
+            <div className="about-me-hex-left">
             <Carousel/>
-            <img src={AboutMeLeft} alt="background Hexagons"></img>
-            <div>
-
-                <div className="about-me-content">
-                    <p>My journey hasn’t been a straight path. In high school, I was active in cheer and dance but had no idea how to eat healthy. I was a vegetarian living off pasta and sugar, and while my teenage metabolism and active lifestyle kept me at a healthy weight, those habits caught up with me in college. Between working multiple jobs, school, and stress eating, I gained 52 pounds by the time I graduated. In 2019, I decided to make a change. I focused on building better habits, like consistent exercise and mindful eating, instead of chasing quick fixes. Over time, I lost 35 pounds and fell in love with bodybuilding, earning first place and professional athlete status in my first WBFF competition. But I realized that extreme competition prep wasn’t sustainable—it left me burned out, struggling with food guilt, and resenting the gym.</p>
-                </div>
-                <div>
-                    <h1 className="about-me-title">About Capressa</h1>
-                </div>
-
             </div>
-            <img src={AboutToPlans} alt="background Hexagons"></img>
+            {/*<img className="about-me-hex-left" src={AboutMeLeft} alt="background Hexagons"></img>*/}
+
+            {/*<img src={AboutToPlans} alt="background Hexagons"></img>*/}
+            <div className="about-me-hex-left-right">
+                <img className="aboutcapressa" src={aboutcapressa} alt="background Hexagons"></img>
+            </div>
         </div>        
     )
 }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,121 @@
-import React from 'react';
-import '.././App.css';
+import React, { useState } from 'react';
+import '../App.css';
+//import aboutmebubble from '.././assets/aboutmebubble.svg';
 
 function Carousel() {
-    return (
-        <div className="carousel">
-            <svg xmlns="http://www.w3.org/2000/svg" className="arrow-up" width="36.428" height="14.725" viewBox="0 0 36.428 14.725"><path id="Icon_ion-md-arrow-dropright" data-name="Icon ion-md-arrow-dropright" d="M0,0,14.725,18.214,0,36.428Z" transform="translate(0 14.725) rotate(-90)" fill="#ed3a72"/></svg>
 
-            <svg xmlns="http://www.w3.org/2000/svg" className="carousel-hex .carousel-hex-active" width="56.429" height="48.868" viewBox="0 0 56.429 48.868"><path id="Path_3972" data-name="Path 3972" d="M39.723,0H13.241L0,22.934,13.241,45.868H39.723L52.964,22.934Z" transform="translate(54.697 47.368) rotate(-180)" fill="#ffe403" stroke="#ed3a72" stroke-miterlimit="10" stroke-width="3"/></svg>
+  const [activeIndex, setActiveIndex] = useState(0);
+  const totalItems = 2;
 
-            <svg xmlns="http://www.w3.org/2000/svg" className="carousel-hex" width="48.954" height="42.395" viewBox="0 0 48.954 42.395"><path id="Path_3973" data-name="Path 3973" d="M31.519,0H10.507L0,18.2,10.507,36.4H31.519L42.026,18.2Z" transform="translate(45.49 39.395) rotate(-180)" fill="#ed3a72" stroke="#ed3a72" stroke-miterlimit="10" stroke-width="6"/></svg>
+  const handelCarouselClick = (direction) => {
+    if (direction === 'next') {
+      setActiveIndex((prevIndex) => (prevIndex + 1) % totalItems); 
+    } else if (direction === 'prev') {
+      setActiveIndex((prevIndex) => (prevIndex - 1 + totalItems) % totalItems); 
+    }
+  };
 
-            <svg xmlns="http://www.w3.org/2000/svg" className="arrow-down" width="36.428" height="14.725" viewBox="0 0 36.428 14.725"><path id="Icon_ion-md-arrow-dropright" data-name="Icon ion-md-arrow-dropright" d="M0,0,14.725,18.214,0,36.428Z" transform="translate(36.428) rotate(90)" fill="#ed3a72"/></svg>
+  return (
+    <div>
+    <div className="carousel">
+      
+      <button onClick={() => handelCarouselClick('prev')}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="arrow-up"
+          width="36.428"
+          height="14.725"
+          viewBox="0 0 36.428 14.725"
+        >
+          <path
+            id="Icon_ion-md-arrow-dropright"
+            data-name="Icon ion-md-arrow-dropright"
+            d="M0,0,14.725,18.214,0,36.428Z"
+            transform="translate(0 14.725) rotate(-90)"
+            fill="#ed3a72"
+          />
+        </svg>
+      </button>
 
-        </div>        
-    )
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="carousel-hex"
+        width="45.49"
+        height="39.395"
+        viewBox="0 0 45.49 39.395"
+      >
+        <path
+          className={activeIndex === 0 ? 'active-path' : 'inactive-path'}
+          id="Path_3973"
+          data-name="Path 3973"
+          d="M31.519,0H10.507L0,18.2,10.507,36.4H31.519L42.026,18.2Z"
+          transform="translate(43.758 37.895) rotate(-180)"
+          fill="#ed3a72"
+          stroke="#ed3a72"
+          strokeMiterlimit="10"
+          strokeWidth="3"
+        />
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="carousel-hex"
+        width="45.49"
+        height="39.395"
+        viewBox="0 0 45.49 39.395"
+      >
+        <path
+          className={activeIndex === 1 ? 'active-path' : 'inactive-path'}
+          id="Path_3973"
+          data-name="Path 3973"
+          d="M31.519,0H10.507L0,18.2,10.507,36.4H31.519L42.026,18.2Z"
+          transform="translate(43.758 37.895) rotate(-180)"
+          fill="#ed3a72"
+          stroke="#ed3a72"
+          strokeMiterlimit="10"
+          strokeWidth="3"
+        />
+      </svg>
+
+      <button onClick={() => handelCarouselClick('next')}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="arrow-down"
+          width="36.428"
+          height="14.725"
+          viewBox="0 0 36.428 14.725"
+        >
+          <path
+            id="Icon_ion-md-arrow-dropright"
+            data-name="Icon ion-md-arrow-dropright"
+            d="M0,0,14.725,18.214,0,36.428Z"
+            transform="translate(36.428) rotate(90)"
+            fill="#ed3a72"
+          />
+        </svg>
+      </button>
+    </div>
+    <div className={activeIndex === 0 ? 'test-act' : 'test-inc'}>
+                {/*<img src={aboutmebubble} alt="about me speech bubble"></img>*/}
+                <div className="about-me-content">
+                    <p className="about-me-content-text">My journey hasn’t been a straight path. In high school, I was active in cheer and dance but had no idea how to eat healthy. I was a vegetarian living off pasta and sugar, and while my teenage metabolism and active lifestyle kept me at a healthy weight, those habits caught up with me in college. Between working multiple jobs, school, and stress eating, I gained 52 pounds by the time I graduated.<br></br> In 2019, I decided to make a change. I focused on building better habits, like consistent exercise and mindful eating, instead of chasing quick fixes. Over time, I lost 35 pounds and fell in love with bodybuilding, earning first place and professional athlete status in my first WBFF competition. But I realized that extreme competition prep wasn’t sustainable—it left me burned out, struggling with food guilt, and resenting the gym.</p>
+                </div>
+                <div>
+                    <h1 className="about-me-title">About Capressa</h1>
+                </div>
+
+     </div>
+     <div className={activeIndex === 1 ? 'test-act' : 'test-inc'}>
+                <div className="">
+                    <p>content 2</p>
+                </div>
+                <div>
+                    <h1 className="">Before & After</h1>
+                </div>
+     </div>
+    
+    </div>
+  );
 }
 
 export default Carousel;
